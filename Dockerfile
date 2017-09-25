b300db79b8cefd29e4df26338b7b28534e6af6e4
Luca Orlandi <luca.orlandi@gmail.com>
2017-09-25 18:15:32 +0200

    Installed Microsoft sql server command line tools

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM microsoft/mssql-server-linux
 
 ENV ACCEPT_EULA Y
 ENV SA_PASSWORD Password1
+
+# Install command line tools
+RUN apt-get update && apt-get install -y python-pip && apt-get autoclean && pip install mssql-scripter
+
 RUN mkdir /docker-entrypoint-initdb.d
 COPY docker-entrypoint.sh /usr/local/bin/
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ sleep 20
 for f in /docker-entrypoint-initdb.d/*; do
     case "$f" in
         *.sh)     echo "$0: running $f"; . "$f" ;;
-#        *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+        *.sql)    echo "$0: running $f"; /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1,1433 -U sa -P $SA_PASSWORD -i "$f"; echo ;;
 #        *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
         *)        echo "$0: ignoring $f" ;;
     esac
